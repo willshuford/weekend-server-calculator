1e04c2274152f56c83b267706b9a285551a4bb8c
Willie Shuford <willshuford@gmail.com>
2024-12-02 21:04:34 -0600

    client updated

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,35 @@
 console.log('client.js is sourced!');
+
+document.querySelector('button:nth-of-type(5)').onclick = function (event) {
+    event.preventDefault();
+    
+    const numOne = document.getElementById('numOne').value;
+    const numTwo = document.getElementById('numTwo').value;
+    const operator = '+';
+
+    axios.post('/calculations', { numOne, numTwo, operator })
+    .then((response) => {
+        document.getElementById('recentResult').innerHTML += `<p>${response.data.result}</p>`;
+    })
+    .catch((error) => {
+      console.error('Error with POST request:', error);
+    });
+};
+
+function getCalculationHistory() {
+    axios.get('/calculations')
+      .then((response) => {
+        const resultHistory = document.getElementById('resultHistory');
+      resultHistory.textContent = response.data;
+    })
+    .catch((error) => {
+      console.error('Error fetching calculation history:', error);
+    });
+}
+
+getCalculationHistory;
+
+document.querySelector('button:nth-of-type(6)').onclick = function () {
+    document.getElementById('numOne').value = '';
+    document.getElementById('numTwo').value = '';
+};
